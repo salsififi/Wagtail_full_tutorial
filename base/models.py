168c18c040bba5f900ddad56beedf23c0d74abc9
Simon Salvaing <simon.salvaing@gmail.com>
2025-02-28 11:07:46 +0100

    Add contact page

--- base/models.py
+++ base/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PublishingPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PublishingPanel, InlinePanel, FieldRowPanel
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
 from wagtail.fields import RichTextField
 from wagtail.models import DraftStateMixin, PreviewableMixin, RevisionMixin, TranslatableMixin
@@ -47,3 +50,25 @@ class FooterText(
 
     class Meta(TranslatableMixin.Meta):
         verbose_name_plural = "Footer text"
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey("FormPage", on_delete=models.CASCADE, related_name="form_fields")
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel("intro"),
+        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("thank_you_text"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel("from_address"),
+                FieldPanel("to_address"),
+            ]),
+            FieldPanel("subject"),
+        ], "Email")
+    ]
